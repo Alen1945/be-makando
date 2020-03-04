45438f7e773b18857796d7edc80f862909b94ad7
Alen <k45sp3rhow@gmail.com>
2020-03-04 20:46:53 +0700

    unsigned to column id restaurant

--- src/migrations/restaurants.js
+++ src/migrations/restaurants.js
@@ -1,12 +1,12 @@
 const restaurants = `
   CREATE TABLE IF NOT EXISTS restaurants(
-  _id int(11) PRIMARY KEY AUTO_INCREMENT,
-  id_owner INT(11) NOT NULL,
+  _id int(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  id_owner INT(11) UNSIGNED NOT NULL,
   name VARCHAR(40) NOT NULL,
   logo TEXT,
   addres TEXT,
   description TEXT,
-  created_by INT(11),
+  created_by INT(11) UNSIGNED,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   upated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 )
