4b44e0fdcf0398fc18c37361e82e11209a702e81
Alen <k45sp3rhow@gmail.com>
2020-03-04 20:45:29 +0700

    unsigned to column id

--- src/migrations/users.js
+++ src/migrations/users.js
@@ -1,6 +1,6 @@
 const users = `
   CREATE TABLE IF NOT EXISTS users(
-  _id int(11) PRIMARY KEY AUTO_INCREMENT,
+  _id int(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(40) NOT NULL,
   password VARCHAR(100) NOT NULL,
   is_admin  TINYINT(1) DEFAULT 0,
@@ -11,8 +11,8 @@ const users = `
 `
 const userProfile = `
   CREATE TABLE IF NOT EXISTS userProfile(
-    _id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    id_user INT(11) NOT NULL,
+    _id INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id_user INT(11) UNSIGNED NOT NULL,
     fullname VARCHAR(70) NULL,
     email VARCHAR(40) NULL,
     gender ENUM('male','female','other') DEFAULT 'other',
