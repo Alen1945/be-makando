3db77640cb35e1ae898e1cfc2ec78b99e28664d5
Alen <k45sp3rhow@gmail.com>
2020-03-04 07:16:44 +0700

    create db connection

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+require('dotenv').config()
+
+const db = mysql.createConnection({
+  host: process.env.DB_SERVER,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+})
+db.connect()
+
+module.exports = db
