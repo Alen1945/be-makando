d5b32dd14feed5243b7f26aaa37b430ae68c4f1e
Alen <k45sp3rhow@gmail.com>
2020-03-06 08:24:54 +0700

    update get all restaurants

--- src/controllers/restaurants.js
+++ src/controllers/restaurants.js
@@ -1,12 +1,63 @@
+const qs = require('qs')
 const { GetRestaurants, CreateRestaurant, UpdateRestaurant, DeletRestaurant } = require('../models/restaurants')
 const { GetUser } = require('../models/users')
 exports.GetAllRestaurant = async (req, res, next) => {
   try {
-    const dataRestaurants = await GetRestaurants(false, { p: 'ram' })
-    res.status(200).send({
-      success: true,
-      data: dataRestaurants
-    })
+    const params = {
+      currentPage: req.query.page || 1,
+      perPage: req.query.limit || 5,
+      search: req.query.search || '',
+      sort: req.query.sort || [{ key: 'name', value: 0 }]
+    }
+    const column = ['_id', 'name', 'location', 'description']
+    if (req.query.search) {
+      params.search = Object.keys(params.search).map((v, i) => {
+        if (column.includes(v)) {
+          return { key: v, value: req.query.search[v] }
+        } else {
+          return [{ key: 'name', value: 0 }]
+        }
+      })
+    }
+    if (req.query.sort) {
+      params.sort = Object.keys(params.sort).map((v, i) => {
+        if (column.includes(v)) {
+          return { key: v, value: req.query.sort[v] }
+        } else {
+          return { key: 'name', value: 0 }
+        }
+      })
+    }
+    const dataRestaurants = await GetRestaurants(false, params)
+
+    const totalPages = Math.ceil(dataRestaurants.total / parseInt(params.perPage))
+    const query = req.query
+    query.page = parseInt(params.currentPage) + 1
+    const nextPage = (parseInt(params.currentPage) < totalPages ? process.env.APP_URL.concat(`${req.baseUrl}?${qs.stringify(query)}`) : null)
+    query.page = parseInt(params.currentPage) - 1
+    const previousPage = (parseInt(params.currentPage) > 1 ? process.env.APP_URL.concat(`${req.baseUrl}${qs.stringify(query)}`) : null)
+
+    const pagination = {
+      currentPage: params.currentPage,
+      nextPage,
+      previousPage,
+      totalPages,
+      perPage: params.perPage,
+      totalEntries: dataRestaurants.total
+    }
+    if (dataRestaurants.results.length > 0) {
+      res.status(200).send({
+        success: true,
+        data: dataRestaurants.results,
+        pagination
+      })
+    } else {
+      res.status(200).send({
+        success: true,
+        data: false,
+        msg: 'Data is Empty'
+      })
+    }
   } catch (e) {
     console.log(e)
     res.status(202).send({
