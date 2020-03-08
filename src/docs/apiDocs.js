fc9f10edd5a129751530225f126656f5abe86472
Alen <k45sp3rhow@gmail.com>
2020-03-09 01:53:58 +0700

    items docs

--- src/docs/apiDocs.js
+++ src/docs/apiDocs.js
@@ -2,6 +2,6 @@
 module.exports = {
   Guest: ['Authentication.yml', 'GuestBrowse.yml'],
   User: ['Authentication.yml', 'User.yml', 'GuestBrowse.yml', 'Carts.yml', 'reviews.yml'],
-  Admin: ['Authentication.yml'],
+  Admin: ['Authentication.yml', 'GuestBrowse.yml', 'Items.yml'],
   SuperAdmin: ['Authentication.yml']
 }
