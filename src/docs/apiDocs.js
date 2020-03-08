a2befe16f5012603db7758a440fad4b04022649f
Alen <k45sp3rhow@gmail.com>
2020-03-09 00:51:52 +0700

    docs reviews

--- src/docs/apiDocs.js
+++ src/docs/apiDocs.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   Guest: ['Authentication.yml', 'GuestBrowse.yml'],
-  User: ['Authentication.yml', 'User.yml'],
+  User: ['Authentication.yml', 'User.yml', 'reviews.yml'],
   Admin: ['Authentication.yml'],
   SuperAdmin: ['Authentication.yml']
 }
