f036d05133f5746feeb47a5f85e6a2892b654c27
Alen <k45sp3rhow@gmail.com>
2020-03-09 00:03:52 +0700

    general user docs

--- src/docs/apiDocs.js
+++ src/docs/apiDocs.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   Guest: ['Authentication.yml', 'GuestBrowse.yml'],
-  User: ['Authentication.yml'],
+  User: ['Authentication.yml', 'User.yml'],
   Admin: ['Authentication.yml'],
   SuperAdmin: ['Authentication.yml']
 }
